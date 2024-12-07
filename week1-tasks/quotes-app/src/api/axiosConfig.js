b8c86968a15f8e07c06cde913af76941eca1968f
Jay-Patel-0812 <patel.jay.81202@gmail.com>
2024-12-07 18:54:20 +0530

    config changes for frontend

--- week1-tasks/quotes-app/src/api/axiosConfig.js
+++ week1-tasks/quotes-app/src/api/axiosConfig.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:8080"
+    // baseURL: "http://localhost:8080",
+    baseURL:"https://techplement-l6bh.onrender.com",
     // timeout: 5000,
 });
